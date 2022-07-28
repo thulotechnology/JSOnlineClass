62ec56a9883b850366cec8b8600eb38469b10b1b
Bishworaj Poudel <iambrp.tech@gmail.com>
2022-07-28 07:43:34 +0545

    Object

--- 18function.js
+++ 18function.js
@@ -6,11 +6,13 @@ function printName(){
 // function that add 2 numbers
 // a and b are parameters
 // Parameter No Return Type
+
 function add(a, b){
    let sum = a + b;
    console.log(`The sum is ${sum}`);
 }
 // Parameter Return Type
+// function that takes p / t / r as parameter and return simple interest
 function sum(num1, num2){
     return num1 + num2;
 }
@@ -35,4 +37,4 @@ if(personAge >= voterAge()){
     console.log("You are not eligible to vote");
 }
 
-// 
+
---
+++ 19object.js
@@ -0,0 +1,11 @@
+// You can store multiple values of the thing.
+
+const student = {
+    firstName: "Rajan",
+    lastName: "Poudel",
+    age: 29,
+    hobbies: ["Coding", "Reading", "Watching Movies"],
+    rollNo: "18-BIT-099",
+}
+
+console.log(student.hobbies[2]);
